3092e21473d74d2bb07bb96e1f7762920e220ece
PayamS <72343921+Payam-Shaker@users.noreply.github.com>
2021-12-31 17:53:26 +0100

    foundation of crud app

---
+++ resources/views/create.blade.php
@@ -0,0 +1,27 @@
+<div class="card uper">
+    <div class="card-header">
+        Add Product
+    </div>
+    <div class="card-body">
+        <!-- @if ($errors->any())
+      <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+              <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+      </div><br />
+    @endif -->
+        <form method="post" action="{{ route('products.store') }}">
+            <div class=" form-group">
+                @csrf
+                <label for="country_name">Title:</label>
+                <input type="text" class="form-control" name="title" />
+            </div>
+            <div class="form-group">
+                <label for="cases">Price :</label>
+                <input type="text" class="form-control" name="price" />
+            </div>
+            <button type="submit" class="btn btn-primary">Add product</button>
+        </form>
+    </div>
